699869b118eab3d3eb0c74a363512b617c9b5058
MofuMofu2 <froakie002@gmail.com>
2019-03-07 21:25:50 +0900

    [add] add code

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c" rel="stylesheet">
+    <app-header></app-header>
+    <div class="book-stock-data">
+      <book-stock-data></book-stock-data>
+    </div>
+    <div class="balance-data">
+      <input-balance-data></input-balance-data>
+    </div>
+  </div>
+</template>
+
+<script>
+import 'normalize.css'
+import AppHeader from './components/AppHeader.vue'
+import BookStockData from './components/BookStockData.vue'
+import InputBalanceData from './components/InputBalanceData.vue'
+
+export default {
+  name: 'app',
+  components: {
+    'app-header': AppHeader,
+    'input-balance-data': InputBalanceData,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "M PLUS Rounded 1c";
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.main-contents {
+  align-items: center;
+  display: flex;
+}
+</style>
---
+++ src/components/BookStockData.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="book-stock-data">
+    <table class="input-table">
+        <tr class="table-header">
+          <th class="book-name">タイトル</th>
+          <th class="order">注文数</th>
+          <th class="deliverly-books">納入冊数</th>
+          <th class="free-distribution">無料配付</th>
+          <th class="book-stock">在庫</th>
+          <th class="sales-book-">頒布数</th>
+        </tr>
+        <tr class="input-box">
+          <td><input type="text" class="book-name-text" v-model="bookName"></td>
+          <td><input type="number" class="order-" v-model="order"></td>
+          <td><input type="number" class="deliverly-books-" v-model="deliverBooks"></td>
+          <td><input type="number" class="free-distribution-" v-model="freeDistribution"></td>
+          <td><input type="number" class="book-stock-number" v-model="bookStock"></td>
+          <td><p class="sales-book-result"> {{ salesBook }} </p></td>
+        </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BookStockData',
+  data() {
+    return {
+      bookName: '',
+      order: '',
+      deliverBooks: '',
+      freeDistribution: '',
+      bookStock: '',
+    };
+  },
+  computed: {
+    salesBook() {
+      const sales = (this.deliverBooks - this.freeDistribution) - this.bookStock;
+      return sales;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+table {
+  align-items: center;
+  width: 80vw;
+}
+</style>
